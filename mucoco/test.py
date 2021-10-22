b49c2bc7c349a4953571018bd70513b612f808e1
Sachin Kumar <kumar.sachin52@gmail.com>
2021-10-21 23:21:47 -0400

    updated gitignore

--- mucoco/test.py
+++
@@ -1,4 +0,0 @@
-import mucoco.losses as losses
-
-def main():
-    print(losses.build_loss('wmd', None, None, None))
--- mucoco/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from mucoco.utils.lambdas import Lambda
-from mucoco.utils.targets import TargetProbability, TargetSimplex
-from mucoco.utils.optim import Optimizer
-from mucoco.utils.misc import get_epsilon
